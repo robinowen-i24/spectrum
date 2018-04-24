5fa9a8a786047ff5c95ef1251cd7307e6a11fd4b
Sherrell <voe31998@i24-ws001.diamond.ac.uk>
2018-04-24 18:04:14 +0100

    added intial wrapper script

---
+++ spectrum.py
@@ -0,0 +1,18 @@
+import sys
+#import argparse
+#decide between argparse or sys method or file method
+
+#import other scripts
+#import file_scraper
+#import sum_spectra
+#import possible_emissions
+# import mapme
+
+def main(*args):
+    pass
+     
+if __name__ == '__main__':
+   main()
+
+
+
