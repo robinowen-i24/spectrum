5a9ba1f84d49e6759f4ee141d9d1e1ce3437d675
Sherrell <voe31998@i24-ws006.diamond.ac.uk>
2018-05-03 13:14:52 +0100

    trying to add many small old changes from a dead branch

--- spectrum.py
+++ spectrum.py
@@ -200,7 +200,6 @@ def run(fid, minimum_nrg, incident_nrg, cutoff, mapme, spread, offset, include_l
     plt.show()
 
 def main(args):
-    print 'start2'
     fid          = args.input_file
     minimum_nrg  = args.minimum_energy
     incident_nrg = args.incident_energy
